5b6d2fb343de47c921d65466a595b154d0aeceb9
Lim Yi Xuan <limyixuan12345@gmail.com>
2025-01-16 22:16:47 +0800

    Update nextConfig and logo.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/ui/globals.css";
+import "@/components/globals.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
 
